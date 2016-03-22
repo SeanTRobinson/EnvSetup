8a4c1170d7df12e80b85199ee0c05c36d0e3aaad
Sean Robinson <srobinson@sorensonmedia.com>
2016-03-22 11:48:15 +0000

    Updating mac_setup with echo

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,11 +1,18 @@
+echo "Installing Xcode"
 xcode-select --install
+
+echo "Installing Homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
 brew update
+
+echo "Setting up Git"
 brew install git
 git config --global user.name "Sean Robinson"
 git config --global user.email "Sean@SeanTRobinson.co.uk"
+
+echo "Configuring Ruby"
 echo "gem: --no-document" >> ~/.gemrc
 curl -L https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable
 rvm -v
@@ -13,12 +20,19 @@ ruby -v
 brew doctor
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source ~/.bash_profile
+
+"Installing Fish shell"
 brew install fish
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
-#mkdir -p ~/.config/fish
+mkdir -p ~/.config/fish
+
+echo "Installing Node"
 brew install node
 npm install -g grunt-cli
+npm install -g phantomjs
+npm install -g mocha
+
 fish_update_completions
 #echo "set -g -x fish_greeting ''" >> ~/.config/fish/config.fish
 #echo "set PATH ~/Documents/Development/Android_SDK/sdk/platform-tools/ $PATH" >> ~/.config/fish/config.fish
