e5b81fced3f9c2a28a912a7670bfbdcdde1cfdcc
Sean Robinson <srobinson@sorensonmedia.com>
2016-04-05 14:45:26 +0100

    Added atom packages install

---
+++ atom_setup.sh
@@ -0,0 +1,15 @@
+apm install minimap
+apm install atom-beautify
+apm install file-icons
+apm nstall pigments
+apm install editorconfig
+apm install todo-show
+apm install highlight-selected
+apm install pandoc
+apm install rst-preview-pandoc
+apm install markdown-preview-pandoc
+apm install linter-htmlhint
+apm install linter-jshint
+apm install javascript-snippets
+apm install seti-ui
+apm install atom-material-ui
